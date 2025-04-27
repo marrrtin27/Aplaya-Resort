6deef759e555ddfe15c091d5e20ba47dd76e1c8a
marrrtin27 <80947985+marrrtin27@users.noreply.github.com>
2025-04-28 03:03:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let menubar = document.querySelector(".fa-bars");
+let nav = document.querySelector(".nav");
+
+let xmark = document.querySelector(".fa-xmark");
+
+menubar.addEventListener("click", function () {
+  nav.classList.toggle("show");
+});
+
+xmark.addEventListener("click", function () {
+  nav.classList.toggle("show");
+});
+
+function goToProductDetails(roomName) {
+  window.location.href = `/room-details.html?id=${roomName}`;
+  console.log(roomName);
+}
